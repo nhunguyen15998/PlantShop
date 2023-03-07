c3bfc01566e27a9c65c66f7bc6f6a53422edb86e
Nhu nguyen <aishiteru1509@gmail.com>
2023-03-07 17:32:56 +0700

    updated

--- src/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -21,6 +21,7 @@ using Microsoft.Extensions.Logging;
 
 namespace PlantShop.Areas.Identity.Pages.Account
 {
+    [AllowAnonymous]
     public class RegisterModel : PageModel
     {
         private readonly SignInManager<IdentityUser> _signInManager;
